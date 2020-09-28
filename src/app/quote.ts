93e1eb55920e96625afe57e1411771211d78d9a1
BihawaM <bihawam271@gmail.com>
2020-09-28 14:39:51 +0300

    add quote.ts

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
-// export class Quote {
-//    showQuote: boolean;
-//    constructor(public name: string, public quote: string, public completeDate:  Date){
-//        this.showQuote = false;
-//    }
-// }
+export class Quote {
+   showQuote: boolean;
+   constructor(public name: string, public quote: string, public completeDate:  Date){
+       this.showQuote = false;
+   }
+}
